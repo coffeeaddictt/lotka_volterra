d2bccbdb3cf6e13412c37f0cd0495674bf309999
coffeeaddictt <666catpoop666@gmail.com>
2025-01-10 00:11:54 +0100

    fixing runtaime issue

--- lotka_volterra.py
+++ lotka_volterra.py
@@ -19,4 +19,17 @@ for i in range(1000):
     new_value_time = time[-1] + step
     new_value_lapin = (lapin[-1] * (alpha - beta * renard[-1])) * step + lapin[-1]
     new_value_renard = (renard[-1] * (delta * lapin[-1] - gama)) * step + renard[-1]
-    
+
+
+#fixing the runtime error
+    max_population = 1e6
+    new_value_lapin = max(0, min(new_value_lapin, max_population))
+    new_value_renard = max(0, min(new_value_renard, max_population))
+
+
+   
+    time.append(new_value_time)
+    lapin.append(new_value_lapin)
+    renard.append(new_value_renard)
+
+
